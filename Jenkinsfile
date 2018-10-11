526093a41ef3a015aa30aa242ddd34e8445cbf50
orangeTea <cjchow@orangeTeas-MacBook-Pro.local>
2018-10-10 19:32:59 -0700

    asdf

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,14 +9,38 @@ node{
    }
       
    //Configure and publish to artifactory
-  // stage('Artifactory deployment')
- //  {
- //      deployment.artifactoryDeploy()
- //  }
+   stage('Artifactory deployment')
+   {
+   	def releaseLibRes = 'libs-release'
+    def snapshotLibRes = 'libs-snapshot'
+    def releaseLibDep = 'libs-release-local'
+    def snapshotLibDep = 'libs-snapshot-snapshot-local'
+    def pomFilename = 'pom.xml'
+    def goalsVal ='clean install'
+    
+		try{
+       		deployment.artifactoryDeploy(releaseLibRes,snapshotLibRes, releaseLibDep, snapshotLibDep,pomFilename,goalsVal)
+ 		catch(Exception e)
+ 		{
+ 			echo "Artifactory deployment process failed."
+ 		}
+	}
    
    //Send email 
    stage('send email')
    {
-       deployment.sendEmail()
+     def subjectVar = 'Jenkins deployment to Artifactory'
+     def toVar = 'mgchow5007@gmail.com'
+     def bodyVar = 'The artifacts have been published'
+     def fromVar = 'Jenkins'
+     def mimeTypeVar = 'text/html'
+     
+   	   try{
+       		deployment.sendEmail(subjectVar,toVar,bodyVar,fromVar,mimeTypeVar)
+       }
+       catch(Exception e)
+       {
+       		echo "Failed to send an email concerning Artifactory deployment"
+       }
    }
 }
--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -1,18 +1,18 @@
 #!/usr/bin/env groovy
-def artifactoryDeploy()
+def artifactoryDeploy(def releaseLibRes, def snapshotLibRes, def releaseLibDep, def snapshotLibDep, def pomFilename, def goalsVal)
 {
 	def artfactoryServer = Artifactory.server("artifactory")
     def mavenBuild =Artifactory.newMavenBuild()
     env.MAVEN_HOME = "${tool 'maven'}"
+    
+	//def releaseLibRes = 'libs-release'
+    //def snapshotLibRes = 'libs-snapshot'
 
-    def releaseLibRes = 'libs-release'
-    def snapshotLibRes = 'libs-snapshot'
+   // def releaseLibDep = 'libs-release-local'
+    //def snapshotLibDep = 'libs-snapshot-snapshot-local'
 
-    def releaseLibDep = 'libs-release-local'
-    def snapshotLibDep = 'libs-snapshot-snapshot-local'
-
-    def pomFilename = 'pom.xml'
-    def goalsVal ='clean install'
+    //def pomFilename = 'pom.xml'
+    //def goalsVal ='clean install'
 
     mavenBuild.resolver server: artfactoryServer, releaseRepo: releaseLibRes, snapshotRepo: snapshotLibRes
     mavenBuild.deployer server: artfactoryServer, releaseRepo: releaseLibDep , snapshotRepo: snapshotLibDep
@@ -20,13 +20,13 @@ def artifactoryDeploy()
     server.publishBuildInfo projectBuildInfo
 }
 
-def sendEmail()
+def sendEmail(def subjectVar, def toVar, def bodyVar, def fromVar, def mimeTypeVar)
 {
-	 def subjectVar = 'Jenkins deployment to Artifactory'
-     def toVar = 'mgchow5007@gmail.com'
-     def bodyVar = 'The artifacts have been published'
-     def fromVar = 'Jenkins'
-     def mimeTypeVar = 'text/html'
+	 //def subjectVar = 'Jenkins deployment to Artifactory'
+     //def toVar = 'mgchow5007@gmail.com'
+     //def bodyVar = 'The artifacts have been published'
+     //def fromVar = 'Jenkins'
+     //def mimeTypeVar = 'text/html'
 
      emailext(
         subject:subjectVar,
