925b51b775d152933a3e6fa2ecc7baef773ae734
orangeTea <cjchow@orangeTeas-MacBook-Pro.local>
2018-10-10 17:27:12 -0700

    dd

---
+++ src/main/java/vars/artifactoryDeploy.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/env groovy
+def call()
+{
+    def artfactoryServer = Artifactory.server("artifactory")
+    def mavenBuild =Artifactory.newMavenBuild()
+    env.MAVEN_HOME = "${tool 'maven'}"
+
+    def releaseLibRes = 'libs-release'
+    def snapshotLibRes = 'libs-snapshot'
+
+    def releaseLibDep = 'libs-release-local'
+    def snapshotLibDep = 'libs-snapshot-snapshot-local'
+
+    def pomFilename = 'pom.xml'
+    def goalsVal ='clean install'
+
+    mavenBuild.resolver server: artfactoryServer, releaseRepo: releaseLibRes, snapshotRepo: snapshotLibRes
+    mavenBuild.deployer server: artfactoryServer, releaseRepo: releaseLibDep , snapshotRepo: snapshotLibDep
+    def projectBuildInfo=mavenBuild.run pom: pomFilename , goals: goalsVal
+    server.publishBuildInfo projectBuildInfo
+}
+
+
