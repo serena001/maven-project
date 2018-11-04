a21ba85d3ec0fa1b6124119e893d2d04fe8d099b
orangeTea <cjchow@orangeTeas-MacBook-Pro.local>
2018-11-03 17:09:54 -0700

    added changes

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -2,68 +2,61 @@
 
 import groovy.json.JsonSlurperClassic
 
-def artifactoryDeploy(artifactoryPublishConfig,artfactoryServer,mavenBuild,mavenBuildConfig)
+def artifactoryDeployResolveConfig(artifactoryConfig,mavenBuild)
 {
-	//def artifactoryServer = Artifactory.server("artifactory")
-	//def mavenBuild = Artifactory.newMavenBuild()
-	env.MAVEN_HOME = "${tool 'maven'}"
-	
-	def artifactoryDeployMap = new JsonSlurperClassic().parseTest(artifactoryPublishConfig)
-	
-	def releaseLibRes = artifactoryDeployMap.releaseLibRes
-	def snapshotRepo = artifactoryDeployMap.snapshotRepo
-	def snapshotLibRes = artifactoryDeployMap.snapshotLibRes
-	def releaseLibDep = artifactoryDeployMap.releaseLibDep
-	def snapshotLibDep = artifactoryDeployMap.snapshotLibDep
-	def pomFilename = artifactoryDeployMap.pomFilename
-	def goalsVal = artifactoryDeployMap.goalsVal
-	
-	//mavenBuild.resolver server: artfactoryServer, releaseRepo: releaseLibRes, snapshotRepo: snapshotLibRes
-	mavenBuild.deployer server: artfactoryServer, releaseRepo: releaseLibDep, snapshotRepo: snapshotLibDep
-	def projectBuildInfo=mavenBuild.buildMaven(mavenBuild,mavenBuildConfig)
-	artfactoryServer.publishBuildInfo projectBuildInfo	
-}
-
-def createMavenBuildInstance()
-{
-   def mavenBuild = Artifactory.newMavenBuild()
-   return mavenBuild
-}
-
-def createArtifactoryInstance(artifactoryConfig)
-{
-	def artifactoryDeployMap = new JsonSlurperClassic().parseTest(artifactoryConfig)
-	def artifactoryServer = Artifactory.server(artifactoryDeployMap.name)
-	return artifactoryServer
+	env.MAVEN_HOME = "${tool 'maven'}"	
+	def artifactoryConfigMap = new JsonSlurperClassic().parseTest(artifactoryConfig)
+	def mavenBuildMap = new JsonSlurperClassic().parseText(mavenBuildConfig)	
+	mavenBuild.resolver server: artfactoryServer, releaseRepo: artifactoryConfigMap.releaseLibRes, snapshotRepo: artifactoryConfigMap.snapshotLibRes
+	mavenBuild.deployer server: artfactoryServer, releaseRepo: artifactoryConfigMap.releaseLibDep, snapshotRepo: artifactoryConfigMap.snapshotLibDep
+	return mavenBuild
 }
 
 def archiveArtifact(archiveConfig)
 {
 	def archiveMap = new JsonSlurperClassic().parseText(archiveConfig)
-	def archiveSrc = archiveMap.archiveSrc
-	archiveArtifacts artifacts: archiveSrc, fingerprint: true, OnlyIfSuccessful:true
+	archiveArtifacts artifacts: archiveMap.archiveSrc, fingerprint:archiveMap.fingerprint, OnlyIfSuccessful:archiveMap.OnlyIfSuccessful
 }
 
 def sendEmail(emailConfig)
 {
-	def emailMap = new JsonSlurperClassic().parseText(emailConfig)
-	def subject = emailMap.subject
-	def to = emailMap.to
-	def body = emailMap.body
-	def from = emailMap.from
-	def mimeType = emailMap.mimeType
-	
+	def emailMap = new JsonSlurperClassic().parseText(emailConfig)	
 	emailext(
-		subject:subject,
-		to:to,
-		body:body,
-		from:from,
-		mimeType:mimeType
+		subject:emailMap.subject,
+		to:emailMap.to,
+		body:emailMap.body,
+		from:emailMap.from,
+		mimeType:emailMap.mimeType
 		)
 }
-def buildMaven(mavenBuild,mavenBuildConfig)
+
+def createArtifactoryInstance()
 {
-	def mavenBuildMap = new JsonSlurperClassic().parseText(mavenBuildConfig)
-	env.MAVEN_HOME = "${tool 'maven'}"
-	def projectBuildInfo=mavenBuild.run pom: mavenBuildMap.pomFilename, goals:mavenBuildMap.goalsVal
+	def artifactoryServer = Artifactory.server("artifactory")
+	return artifactoryServer
 }
+
+def createMavenBuildInstance()
+{
+	env.MAVEN_HOME = "${tool 'maven'}"
+	def mavenBuild = Artifactory.newMavenBuild()
+	return mavenBuild
+}
+
+def buildMaven(mavenBuild,pomFileName,goals)
+{
+	def projectBuildInfo=mavenBuild.run pom:pomFileName,goals:goals
+	return projectBuildInfo
+}
+
+def artifactoryPromoteInteractive(artfactoryServer,artifactoryPromoteConfig,displayName)
+{
+	artfactoryServer.addInteractivePromotion server:artfactoryServer,promotionConfig:artifactoryPromoteConfig, displayName:displayName
+}
+
+def updatePOMVersion(versionNumber){
+	def verionNumberArry = (versionNumber.split("-"))[0].split(".")
+	def versionNumberNew = versionNumberArry[0] + "." + versionNumberArry[1] + "." + (versionNumberArry[2] + 1) + "-" + verionNumberArry[3]
+	return versionNumber
+}
+
