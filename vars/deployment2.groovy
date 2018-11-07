78611b2ad48dee0efbd2be2721a4c6e8b1edd9da
seren <mgchow5007@gmail.com>
2018-11-06 16:43:07 -0800

    added

---
+++ vars/deployment2.groovy
@@ -0,0 +1,76 @@
+#!/usr/bin/env groovy
+
+import groovy.json.JsonSlurperClassic
+
+def artifactoryDeployResolveConfig(artifactionDeployResolveConfig,mavenBuild)
+{
+	env.MAVEN_HOME = "${tool 'maven'}"
+	def deployer = [	server: artifactionDeployResolveConfig.artfactoryServer, releaseRepo: artifactionDeployResolveConfig.releaseLibDep, snapshotRepo: artifactionDeployResolveConfig.snapshotLibDep]
+	mavenBuild.resolver server: artifactionDeployResolveConfig.artfactoryServer, releaseRepo: artifactionDeployResolveConfig.releaseLibRes, snapshotRepo: artifactionDeployResolveConfig.snapshotLibRes
+	//mavenBuild.deployer server: artifactionDeployResolveConfig.artfactoryServer, releaseRepo: artifactionDeployResolveConfig.releaseLibDep, snapshotRepo: artifactionDeployResolveConfig.snapshotLibDep
+	mavenBuild.deployer deployer
+	return mavenBuild
+}
+
+def archiveArtifact(archiveConfig)
+{
+	def archiveMap = new JsonSlurperClassic().parseText(archiveConfig)
+	archiveArtifacts artifacts: archiveMap.archiveSrc, fingerprint:archiveMap.fingerprint, OnlyIfSuccessful:archiveMap.OnlyIfSuccessful
+}
+
+def sendEmail(emailConfig)
+{
+	def emailMap = new JsonSlurperClassic().parseText(emailConfig)	
+	emailext(
+		subject:emailMap.subject,
+		to:emailMap.to,
+		body:emailMap.body,
+		from:emailMap.from,
+		mimeType:emailMap.mimeType
+		)
+}
+
+def createArtifactoryInstance()
+{
+	def artifactoryServer = Artifactory.server("artifactory")
+	return artifactoryServer
+}
+
+def createMavenBuildInstance()
+{
+	env.MAVEN_HOME = "${tool 'maven'}"
+	def mavenBuild = Artifactory.newMavenBuild()
+	return mavenBuild
+}
+
+def buildMaven(mavenBuild,pomFileName,goals)
+{
+	def projectBuildInfo=mavenBuild.run pom:pomFileName,goals:goals
+	return projectBuildInfo
+}
+
+def artifactoryPromoteInteractive(artfactoryServer,artifactoryPromote,displayName)
+{
+	artfactoryServer.addInteractivePromotion server:artfactoryServer,promotionConfig:artifactoryPromoteConfig, displayName:displayName
+}
+
+def artifactoryPromote(artifactoryPromoteConfig)
+{
+	def artifactoryPromoteMap = new JsonSlurperClassic().parseText(artifactoryPromoteConfig)
+	def promotionConfigs=[
+	"buildName":artifactoryPromoteMap.buildName,
+	"buildNumber":artifactoryPromoteMap.buildNumber,
+	"targetRepo":artifactoryPromoteMap.targetRepo,
+	"sourceRepo":artifactoryPromoteMap.sourceRepo,
+	"status":artifactoryPromoteMap.status,
+	"copy":artifactoryPromoteMap.copy,
+	"failfast":artifactoryPromoteMap.failFast]
+	return promotionConfigs
+}
+
+def updatePOMVersionNumber(versionNumber){
+	def versionNumberArry = (versionNumber.split("-"))[0].split(".")
+	def versionNumberNew = versionNumberArry[0] + "." + versionNumberArry[1] + "." + (versionNumberArry[2] + 1) + "-" + verionNumberArry[3]
+	return versionNumber
+}
+
